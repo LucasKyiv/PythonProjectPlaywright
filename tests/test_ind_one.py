b692dff071d5b4fe6ce1a52e713426389899b056
Ihor Nazarkin <ihornazarkin@gmail.com>
2025-08-14 22:24:10 +0100

    adding second title

---
+++ tests/test_ind_one.py
@@ -0,0 +1,15 @@
+import re
+import time
+
+from playwright.sync_api import expect, Page
+
+
+def test_google_search(page: Page):
+    page.goto('https://www.google.com/ncr')
+    page.locator("//div[text()='Accept all']").click()
+    page.get_by_role("combobox").fill("OpenAI")
+    time.sleep(2)
+    page.keyboard.press("Escape")
+    page.screenshot(path="before_click.png")
+    page.get_by_role("button", name="Google Search").click(timeout=5000)
+    expect(page).to_have_title(re.compile("OpenAI", re.IGNORECASE))
